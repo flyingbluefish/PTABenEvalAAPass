0c3e81a8b3cdea3f2a0f3ca642d6f3d7482bec75
hotpeperoncino <bluenemophila@gmail.com>
2016-12-16 10:38:27 +0900

    change score name (maybebug->buggy)

--- lib/PTABenEvalAAPass.cpp
+++ lib/PTABenEvalAAPass.cpp
@@ -62,7 +62,7 @@ static char ID;
  	    else if (aares == MayAlias)
               score = "inadequate";
 	    else if (aares == MustAlias)
-              score = "maybebug";
+              score = "buggy";
             else
               score = "unknown";
 	  } else if (fun.equals(MAYALIAS)) {
